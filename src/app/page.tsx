ddbad4c01835eca775160cf16e1ac5726e173b58
Kristaps-M <kristapsmitins@gmail.com>
2024-03-03 13:58:08 +0200

    update. Add messages, fix empty array errors, fix clickedUserID === null errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,45 @@ interface IUser {
   id: number;
   fullName: string;
   profileImage: string;
+  theMessages: string[];
 }
 
 export default function Home() {
   let usersList = [
-    { id: 1, fullName: "Janis Berzins", profileImage: "1.jpg" },
-    { id: 2, fullName: "Karlis Lielais", profileImage: "2.jpg" },
-    { id: 3, fullName: "Baiba Egle", profileImage: "3.jpg" },
-    { id: 4, fullName: "Sandra Ozola", profileImage: "4.jpg" },
+    {
+      id: 1,
+      fullName: "Janis Berzins",
+      profileImage: "1.jpg",
+      theMessages: ["hello 1", "how?"],
+    },
+    {
+      id: 2,
+      fullName: "Karlis Lielais",
+      profileImage: "2.jpg",
+      theMessages: ["hello 2", "how?"],
+    },
+    {
+      id: 3,
+      fullName: "Baiba Egle",
+      profileImage: "3.jpg",
+      theMessages: ["hello 3", "how?"],
+    },
+    {
+      id: 4,
+      fullName: "Sandra Ozola",
+      profileImage: "4.jpg",
+      theMessages: ["hello 4", "how?"],
+    },
+    {
+      id: 5,
+      fullName: "Ingrida Vienīgā",
+      profileImage: "5.jpg",
+      theMessages: [],
+    },
   ];
   const [theUsers, setTheUsers] = useState<IUser[]>(usersList);
   const [searchUser, setSeachUser] = useState("");
-  const [clickedUserByIndex, setClickedUserByIndex] = useState(0);
-  const [clickedUserID, setClickedUserID] = useState(theUsers[0].id);
+  const [clickedUserID, setClickedUserID] = useState<number | null>(null);
 
   useEffect(() => {
     let tempUsers = [...theUsers];
@@ -30,6 +56,55 @@ export default function Home() {
       setTheUsers(usersList);
     }
   }, [searchUser]);
+
+  function addTextToMessagesWhenEnterPressed(stringPressEnter: string) {
+    let tempUsers = [...theUsers];
+    for (let index = 0; index < tempUsers.length; index++) {
+      if (tempUsers[index].id === clickedUserID) {
+        tempUsers[index].theMessages.push(stringPressEnter);
+      }
+    }
+
+    setTheUsers(tempUsers);
+  }
+
+  function returnClickedUsersFullname() {
+    if (clickedUserID) {
+      if (theUsers.length > 0) {
+        let fullName;
+        try {
+          fullName = theUsers.filter((u) => u.id === clickedUserID)[0].fullName;
+          return fullName;
+        } catch (error) {
+          return "";
+        }
+      } else {
+        return "";
+      }
+    }
+
+    return "";
+  }
+
+  function returnClickedPersonsMessages() {
+    if (clickedUserID !== null) {
+      if (theUsers.length > 0) {
+        try {
+          return theUsers
+            .filter((u) => u.id === clickedUserID)[0]
+            .theMessages.map((m, index) => {
+              return <p key={index}>{m}</p>;
+            });
+        } catch (error) {
+          return <p></p>;
+        }
+      } else {
+        return <p></p>;
+      }
+    }
+
+    return <p></p>;
+  }
 
   return (
     // MAIN = className="flex min-h-screen flex-col items-center justify-between p-24"
@@ -48,10 +123,8 @@ export default function Home() {
         </div>
         <div>
           02
-          {/* Search by array index */}
-          {/* {theUsers[clickedUserByIndex].fullName} */}
           {/* Search by ID */}
-          {theUsers.filter((u) => u.id === clickedUserID)[0].fullName}
+          {returnClickedUsersFullname()}
         </div>
         <div>
           03
@@ -73,7 +146,24 @@ export default function Home() {
             );
           })}
         </div>
-        <div>04</div>
+        <div>
+          04
+          {returnClickedPersonsMessages()}
+          <input
+            type="text"
+            placeholder="Write a message..."
+            onKeyDown={(event) => {
+              if (event.key === "Enter") {
+                const inputElement = event.target as HTMLInputElement;
+                const inputValue = inputElement.value;
+                // Call your function with the input value
+                addTextToMessagesWhenEnterPressed(inputValue);
+                // Clear the input field
+                inputElement.value = "";
+              }
+            }}
+          />
+        </div>
       </div>
     </main>
   );
